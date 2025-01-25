2acc24419831f8df7287f45a8f1e6391f223c1c9
Saichandu090 <marrisaichandu143@gmail.com>
2025-01-25 11:42:36 +0530

    Updated Regex for registering user

--- src/main/java/com/example/bookstore/requestdto/UserRegisterEntity.java
+++ src/main/java/com/example/bookstore/requestdto/UserRegisterEntity.java
@@ -1,9 +1,6 @@
 package com.example.bookstore.requestdto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,18 +15,24 @@ import java.time.LocalDate;
 public class UserRegisterEntity
 {
     @NotNull
-    @Pattern(regexp = "^[A-Z][A-Za-z .]{2,}$",message = "First Name Should start with Capital and AtLeast contain 3 characters")
+    @Pattern(regexp = "^[A-Z][a-zA-Z .,\\'-_=+]{2,}$",message = "First Name Should start with Capital and AtLeast contain 3 characters")
     private String firstName;
 
     @NotNull
-    @Pattern(regexp = "^[A-Z][A-Za-z .]{2,}$",message = "Last Name Should start with Capital and AtLeast contain 3 characters")
+    @Pattern(regexp = "^[A-Z][a-zA-Z .,\\'-_=+]{2,}$",message = "Last Name Should start with Capital and AtLeast contain 3 characters")
     private String lastName;
 
     @Past
     private LocalDate dob;
 
-    @NotNull
-    @Pattern(regexp = "^[a-zA-Z 0-9.@_-]{8,}$",message = "Password should be atleast 8 Characters")
+    @NotBlank(message = "Password should not be empty")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    @Pattern.List({
+            @Pattern(regexp = ".*[a-z].*", message = "Password must contain lowercase letter"),
+            @Pattern(regexp = ".*[A-Z].*", message = "Password must contain uppercase letter"),
+            @Pattern(regexp = ".*\\d.*", message = "Password must contain number"),
+            @Pattern(regexp = ".*[!@#$%^&*()_+\\-=\\[\\]{};:'\",./<>?].*", message = "Password must contain special character")
+    })
     private String password;
 
     @NotNull
